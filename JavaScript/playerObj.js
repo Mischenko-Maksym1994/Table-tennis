47f80882c366442239d497ae2c73aba1d343ce5d
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-02-04 21:48:08 +0200

    add html code

--- JavaScript/playerObj.js
+++ JavaScript/playerObj.js
@@ -1,19 +1,19 @@
-const player = {
-    name: "name",
-    games: [],
-    totalGame: 0,
-    wins: 0,
+// const player = {
+//     name: "name",
+//     games: [],
+//     totalGame: 0,
+//     wins: 0,
 
 
-}
+// }
 
-console.log(player);
+// console.log(player);
 
-const Kolya = Object.create(player);
-Kolya.name = "Kolya";
-Kolya.games = [0, 1, 1, 1, 0];
+// const Kolya = Object.create(player);
+// Kolya.name = "Kolya";
+// Kolya.games = [0, 1, 1, 1, 0];
 
-console.log(Kolya);
+// console.log(Kolya);
 
 
 
