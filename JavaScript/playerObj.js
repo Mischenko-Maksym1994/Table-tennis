f24e2721403af555a2ae1869ef71628a268ff6c1
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-02-04 21:33:06 +0200

    Add random function

--- JavaScript/playerObj.js
+++ JavaScript/playerObj.js
@@ -14,3 +14,18 @@ Kolya.name = "Kolya";
 Kolya.games = [0, 1, 1, 1, 0];
 
 console.log(Kolya);
+
+
+
+
+const minValue = 1;
+const maxValue = 45;
+
+function randomTeamNumber() {
+    const minValue = 1;
+    const maxValue = 45;
+    return Math.random() * (maxValue - minValue) + minValue;
+}
+
+console.log(randomTeamNumber());
+
