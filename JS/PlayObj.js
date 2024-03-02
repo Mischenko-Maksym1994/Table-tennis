09289daf797172e6d21bbd9c85f522513c911edd
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-03-02 11:12:03 +0200

    add runction to create team

--- JS/PlayObj.js
+++ JS/PlayObj.js
@@ -11,6 +11,64 @@ const gamesDay = [];
 console.log(`Всего игроков ${playersName.length}`);
 console.log(playersName);
 
+
+const toUpperCasePlayers = playersName.map(player => player.toUpperCase());
+const sortNamesPlayers = toUpperCasePlayers.toSorted();
+console.log(sortNamesPlayers);
+
+
+class Team {
+    constructor(player1, player2, arrayGame = [], totalGame=0, wins = 0, winPercentage = 0) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.arrayGame = arrayGame;
+        this.totalGame = totalGame;
+        this.wins = wins;
+        this.winPercentage = winPercentage;
+    }
+}
+///create array with all variable team////
+let arrayTeam = [];
+
+function createTeam(array) {
+        
+    for (let a = 0; a < array.length; a += 1) {
+        // let a = 0;
+        
+        for (let i = 0; i < array.length; i += 1) {
+            if (a < array.length) {
+                if (array[a] !== array[i]) {
+                   
+                    const newT = new Team(); 
+                    newT.player1 = array[a];
+                    newT.player2 = array[i];
+                    arrayTeam.push(newT);
+                    // const newTeam = `${array[a]} ${array[i]}`;
+                    // arrayTeam.push(newTeam); 
+                } else {
+                    continue;
+                } 
+            } else if (a === array.length) {
+                a += 1;
+            }
+        }
+    }
+}
+
+createTeam(sortNamesPlayers);
+console.dir(arrayTeam);
+
+////unique teams////
+const uniqueTeam = [];
+
+// for (element of arrayTeam) {
+//     if (uniqueTeam.includes(element) {
+//         continue;
+//     } )
+// }
+
+////start random number //////
+
 const btnGeneration = document.querySelector(".js-btn");
 const firstNumber = document.querySelector(".js-firstNumber");
 const secondNumber = document.querySelector(".js-secondNumber");
